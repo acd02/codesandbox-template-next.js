1b6d16f4bd2abed925792cde179b48d6c700f2de
acd <alex.acd02@gmail.com>
2022-07-20 15:58:12 +0000

    Update 4 files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/components/**/*.ts',
+    './src/components/**/*.tsx',
+    './src/pages/**/*.ts',
+    './src/pages/**/*.tsx',
+    './src/pagesContent/**/*.ts',
+    './src/pagesContent/**/*.tsx',
+  ],
+}
